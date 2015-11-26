b4c5fcea6310d907bb72b701243c6b03b9dab19f
Mingfei Gao <GaoMF163@163.com>
2015-11-26 14:41:57 +0800

    添加TypeDef模块

---
+++ TypeDef/TypeDef.h
@@ -0,0 +1,69 @@
+/**
+  **************************************************************
+  * @file       TypeDef.h
+  * @author	    高明飞
+  * @version    V1.0
+  * @date       2015-11-23
+  *
+  * @brief      定义一些基本数据类型
+  *
+  * @details 
+  * @verbatim
+  * 程序的详细说明。
+  *
+  * 修改记录：
+  * 2015-09-12 :
+  *   - File Created.
+  * 2015-09-27 :
+  *   - 修改注释格式使其可以使用Doxygen
+  * 2015-11-23 :
+  *   - 进一步修改注释格式
+  *
+  * @endverbatim
+  *
+  * @note
+  * 如果编译平台提供了自己的定义文件，不要使用此文件
+  ***************************************************************
+  */
+
+#ifndef TYPEDEF_H
+#define TYPEDEF_H
+
+#ifndef TYPE_UINT32_T
+#define TYPE_UINT32_T
+/** 32位无符号整数 */
+typedef unsigned int uint32_t;
+#endif
+
+#ifndef TYPE_UINT16_T
+#define TYPE_UINT16_T
+/** 16位无符号整数 */
+typedef unsigned short uint16_t;
+#endif
+
+#ifndef TYPE_UINT8_T
+#define TYPE_UINT8_T
+/** 8位无符号整数 */
+typedef unsigned char uint8_t;
+#endif
+
+#ifndef TYPE_INT32_T
+#define TYPE_INT32_T
+/** 32位有符号整数 */
+typedef signed int int32_t;
+#endif
+
+#ifndef TYPE_INT16_T
+#define TYPE_INT16_T
+/** 16位有符号整数 */
+typedef signed short int16_t;
+#endif
+
+#ifndef TYPE_INT8_T
+#define TYPE_INT8_T
+/** 8位有符号整数 */
+typedef signed char int8_t;
+#endif
+
+#endif
+
